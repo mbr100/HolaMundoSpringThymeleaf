bc2ca7a2bd4719594740a53647046406529533ad
Mario <mario.borrego100@gamil.com>
2022-11-28 19:15:45 +0100

    Inicio de curso

---
+++ src/test/java/com/marioborrego/holamundospring/HolaMundoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marioborrego.holamundospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaMundoSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
